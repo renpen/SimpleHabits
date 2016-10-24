53637c67909d2c34b1d5069e9d046135140e84ff
reneUser <reneUser>
2016-10-24 11:16:38 +0200

    Initial Commit

---
+++ AlarmClock/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AlarmClock
+//
+//  Created by ReneUser on 24.10.16.
+//  Copyright © 2016 ReneUser. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
