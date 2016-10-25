fd6db0851d9dfb3816ecfb8d6faf95476d1fe56a
reneUser <reneUser>
2016-10-25 17:22:35 +0200

    first Commit with rudimentary basic functionality

---
+++ AlarmClock/CalendarTools.swift
@@ -0,0 +1,69 @@
+//
+//  File.swift
+//  AlarmClock
+//
+//  Created by ReneUser on 25.10.16.
+//  Copyright © 2016 ReneUser. All rights reserved.
+//
+
+import Foundation
+import EventKit
+
+let eventStore = EKEventStore()
+class CalendarTools {
+    
+
+    func checkPermission(sender: ViewController)
+    {
+        let status = EKEventStore.authorizationStatus(for: EKEntityType.event)
+        switch(status)
+        {
+        case EKAuthorizationStatus.notDetermined:
+            // First Run
+            getPermission(sender: sender)
+        case EKAuthorizationStatus.authorized:
+            sender.permissionAccepted()
+        case EKAuthorizationStatus.denied,EKAuthorizationStatus.restricted :
+            sender.permissionDenied()
+    }
+}
+
+    private func getPermission(sender: ViewController)
+    {
+        eventStore.requestAccess(to: EKEntityType.event, completion: { (accesGranted, error) in
+            if accesGranted == true {
+                sender.permissionAccepted()     //the Permission asking is asynchronous, so we need to handle it this way when the permission is granted
+            }
+        })
+    }
+    
+func getAllCalendar() -> [EKCalendar]
+    {
+        return eventStore.calendars(for: EKEntityType.event)
+    
+    }
+    
+func getNextWakeUpAppointment(calendar : EKCalendar) -> EKEvent?
+    {
+        let todayDate = Date()
+        var dateComponents = DateComponents()
+        dateComponents.day = 1
+        let userCalendar = Calendar.current
+        let to = userCalendar.date(byAdding: dateComponents, to: todayDate, wrappingComponents: true)
+        let eventsPredicate = eventStore.predicateForEvents(withStart: todayDate,end: to!,calendars: [calendar])
+        let events = eventStore.events(matching: eventsPredicate).sorted(){
+            (e1: EKEvent, e2: EKEvent) -> Bool in
+            return e1.startDate.compare(e2.startDate) == ComparisonResult.orderedAscending
+        }
+        if(events.count == 0)
+        {
+            return nil
+        }
+        else
+        {
+            return events[0]
+        }
+    }
+}
+
+
--- AlarmClock/ViewController.swift
+++ AlarmClock/ViewController.swift
@@ -7,18 +7,77 @@
 //
 
 import UIKit
+import EventKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UIPickerViewDataSource,UIPickerViewDelegate {
 
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var pickShit: UIPickerView!
+    @IBOutlet weak var pickLabel: UILabel!
+    let eventStore = EKEventStore()
+    let cTools = CalendarTools()
+    var calendars : [EKCalendar] = []
+    var events = [EKEvent]()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        cTools.checkPermission(sender: self)
+        pickShit.dataSource = self;
+        pickShit.delegate = self;
+    }
+    
+    func permissionAccepted()
+    {
+        calendars = cTools.getAllCalendar()
+        refreshPicker()
+    }
+    func permissionDenied()
+    {
+                // Show something to the User, that he needs to accept the Calendar and redicrct him to the settings where he can change the settings
+    }
+    
+    func refreshPicker()
+    {
+        pickShit.reloadAllComponents()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    //MARK: - Delegates and data sources
+    //MARK: Data Sources
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return calendars.count
+    }
+    //MARK: Delegates
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return calendars[row].title
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        let event = cTools.getNextWakeUpAppointment(calendar: calendars[row])
+        var title = ""
+        var time = ""
+        if event == nil{
+            title = "Keinen Termin"
+            time = "Keinen Termin"
+        }
+        else
+        {
+            title = event!.title
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "HH-mm"
+            time = dateFormatter.string(from: event!.startDate)
+        }
+        pickLabel.text = title
+        timeLabel.text = time
+    }
+    
 
 
 }
