41131ba21dc8e2b9301b5737b21cfb559ecbb910
reneUser <rene.penkert@gmail.com>
2016-11-09 20:23:17 +0100

    notes corrected

--- xCodeProject/AlarmClock/FunctionTest.swift
+++ xCodeProject/AlarmClock/FunctionTest.swift
@@ -11,15 +11,15 @@ import Foundation
 class FunctionTest {
     
     func testSomething() {
-        var cdh = CoreDataHandler()
-        var alarm = cdh.getAlarmObject()
+        let cdh = CoreDataHandler()
+        let alarm = cdh.getAlarmObject()
         alarm.name = "named alert"
         cdh.save()
-        var alarm2 = cdh.getAlarmObject()
+        let alarm2 = cdh.getAlarmObject()
         alarm2.name = "nemaed alert2"
         cdh.save()
         for savedAlarm in cdh.getAllAlarms()! {
-            var alarm = savedAlarm as! Alarm
+            let alarm = savedAlarm as! Alarm
             print(alarm.name)
         }
     }
