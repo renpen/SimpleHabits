c87ef0a4b7d9032818e4c67bd2505ae205bf074f
Benedikt Bosshammer <bene@Benedikts-MacBook-Pro.local>
2017-04-15 14:37:58 +0200

    ADDED: Viewcontroller classes to new view controllers, night mode views

---
+++ xCodeProject/AlarmClock/SettingsTabViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SettingsTabViewController.swift
+//  AlarmClock
+//
+//  Created by Benedikt Bosshammer on 15.04.17.
+//  Copyright © 2017 ReneUser. All rights reserved.
+//
+
+import UIKit
+
+class SettingsTabViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
