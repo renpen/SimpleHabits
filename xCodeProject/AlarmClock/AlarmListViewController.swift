fc669418c2b9aa58534e14d2c94cffdda323edac
Benedikt Bosshammer <bene@Benedikts-MacBook-Pro.local>
2017-04-18 22:00:07 +0200

    ADDED: Sidemenu, SWReveal as framework, navigation working

---
+++ xCodeProject/AlarmClock/AlarmListViewController.swift
@@ -0,0 +1,43 @@
+//
+//  AlarmListViewController.swift
+//  AlarmClock
+//
+//  Created by Benedikt Bosshammer on 18.04.17.
+//  Copyright © 2017 ReneUser. All rights reserved.
+//
+
+import UIKit
+
+class AlarmListViewController: UIViewController {
+
+    @IBOutlet weak var menuButton: UIBarButtonItem!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if revealViewController() != nil {
+            menuButton.target = revealViewController()
+            menuButton.action = "revealToggle:"
+            view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        }
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ xCodeProject/AlarmClock/HomeScreenViewController.swift
@@ -0,0 +1,44 @@
+//
+//  HomeScreenViewController.swift
+//  AlarmClock
+//
+//  Created by Benedikt Bosshammer on 18.04.17.
+//  Copyright © 2017 ReneUser. All rights reserved.
+//
+
+import UIKit
+
+class HomeScreenViewController: UIViewController {
+    
+    @IBOutlet weak var menuButton: UIBarButtonItem!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.navigationController?.navigationBar.backgroundColor = UIColor.clear
+        
+        if revealViewController() != nil {
+            menuButton.target = revealViewController()
+            menuButton.action = "revealToggle:"
+        }
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
