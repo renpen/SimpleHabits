adfac68dfd8b523abfb58ff5ace0b0683e61c8a1
renpen <rene.penkert@gmail.com>
2017-05-07 22:25:54 +0200

    bugfix StandardAlarm EditAlarm. The TimePicker referred to the on the alarm specified date. That could course bugs because the alarm date could be in the past. See comment in code

--- xCodeProject/AlarmClock/StandardAlarmEditingViewController.swift
+++ xCodeProject/AlarmClock/StandardAlarmEditingViewController.swift
@@ -18,7 +18,17 @@ class StandardAlarmEditingViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         textField.text = alarm.name
-        wakeTimePicker.date = alarm.wakeUpTime!
+        // Just getting the alarm.wakeUpTime for the picker is not correct because the date of the alarm could be in the future or in the past and with the picker you only change hours and minutes -> the alarm will not ring because the date is in the past. 
+        let hour = Calendar.current.component(.hour, from: alarm.wakeUpTime!)
+        let minute = Calendar.current.component(.minute, from: alarm.wakeUpTime!)
+        let calendar = Calendar.current
+        let now = Date()
+        var components = calendar.dateComponents([.year, .month, .day, .hour, .minute, .second], from: now)
+        components.hour = hour
+        components.minute = minute
+        components.second = 0
+        let pickerdate = calendar.date(from: components)!
+        wakeTimePicker.date = pickerdate
     }
     
     @IBAction func savePressed(_ sender: Any) {
